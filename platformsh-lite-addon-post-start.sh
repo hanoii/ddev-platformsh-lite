cc4a0c95e6be8995431a831da352c8a556a4b042
Ariel Barreiro <abarrei@gmail.com>
2023-01-18 17:15:51 -0300

    platform tasks on a file

---
+++ platformsh-lite-addon-post-start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#ddev-generated
+
+FILENAME=/tmp/platform-sh-lite-addon-post-start
+if [ -f "$FILENAME" ] ; then
+  echo "platformsh-lite add-on post start script has already been run!"
+  rm "$FILENAME"
+  exit
+fi
+
+touch $FILENAME
+
+# Update to the latest version
+platform self:update -qy --no-major || true
+
+# Install shell integration to avoid prompt
+platform self:install -qy || true
+
+# Cert load
+([ ! -z "${PLATFORMSH_CLI_TOKEN:-}" ] && platform ssh-cert:load -y) || true
+
+# And create drush aliases
+if [[ "$DDEV_PROJECT_TYPE" == *"drupal"* ]]; then
+  ([ ! -z "${PLATFORMSH_CLI_TOKEN:-}" ] && platform drush-aliases -r -g ${DDEV_PROJECT} -y) || true
+fi
