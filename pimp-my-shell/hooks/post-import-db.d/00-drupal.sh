5cc2af1ba6ea26a58a595f56ceca558df2a1183b
Ariel Barreiro <abarrei@gmail.com>
2024-03-28 11:41:40 -0300

    drush-uli and push-log.sh

--- pimp-my-shell/hooks/post-import-db.d/00-drupal.sh
+++ pimp-my-shell/hooks/post-import-db.d/00-drupal.sh
@@ -12,6 +12,8 @@ if [[ "$DDEV_PROJECT_TYPE" == *"drupal"* ]]; then
   drush cr -y
   drush updb -y
   drush cim -y
+
+  /var/www/html/.ddev/pimp-my-shell/scripts/drush-uli.sh
 else
   gum log --level debug "Not drupal"
 fi
