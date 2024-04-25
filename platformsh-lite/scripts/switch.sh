3c3c54fcf20aae8a6f32d164eb76c7114cfd0786
Ariel Barreiro <abarrei@gmail.com>
2024-04-25 13:18:51 -0300

    switch.sh cleanup

--- platformsh-lite/scripts/switch.sh
+++ platformsh-lite/scripts/switch.sh
@@ -4,9 +4,9 @@
 if [[ "$0" == *"switch.sh"* ]]; then
   gum log --level error The script is meant to be source\'d.
   if [ -n "$IS_FISH_SHELL" ]; then
-    cmd="bass source ./.ddev/platformsh-lite/scripts/switch.sh"
+    cmd="bass source /var/www/html/.ddev/platformsh-lite/scripts/switch.sh"
   else
-    cmd="source ./.ddev/platformsh-lite/scripts/switch.sh"
+    cmd="source /var/www/html/.ddev/platformsh-lite/scripts/switch.sh"
   fi
   gum log Please run \'$cmd\' on your shell
   exit 2
@@ -26,15 +26,7 @@ else
   project_id=${project#*" - "}
 
   gum log --level info --structured "Project selected" project $project
-  if [[ "$0" == *"switch.sh"* ]]; then
-    gum log --level warn The script has been called by the shell so it cannot export environment variable
-    gum style "You can run 'export PLATFORM_PROJECT=' on your shell"
-    gum style "or source this script instead, by doing 'source ./.ddev/platformsh-lite/scripts/switch.sh'"
-    gum log --level warn "It will only be valid in the running shell."
-  else
-    gum log --level info --structured "Variable PLATFORM_PROJECT exported." project $project_id
-    gum log --level warn "It will only be valid in the running shell."
-    export PLATFORM_PROJECT=$project_id
-  fi
+  gum log --level info --structured "Variable PLATFORM_PROJECT exported." project $project_id
+  gum log --level warn "It will only be valid in the running shell."
   export PLATFORM_PROJECT=$project_id
 fi
