64aa45fcdf7b709dd8f7a7cc3c421d5f0fba0f1a
Ariel Barreiro <abarrei@gmail.com>
2024-05-02 09:55:28 -0300

    push-log improvements

--- platformsh-lite/scripts/push-log.sh
+++ platformsh-lite/scripts/push-log.sh
@@ -2,5 +2,17 @@
 #ddev-generated
 set -e -o pipefail
 
-ACTIVITY=$(platform activities --type environment.push --limit 1 --format=plain --columns=id --no-header $@)
-platform activity:log $ACTIVITY
+PENDING=$(platform activities --type environment.push --all --state=pending --format=plain --columns=id --no-header "$@" 2> /dev/null | wc -l)
+
+ACTIVITY=$(platform activities --type environment.push --limit 1 --state=in_progress --format=plain --columns=id --no-header "$@")
+if [ $PENDING -gt 0 ]; then
+  gum log --level warn "There are still $PENDING pending builds"
+  sleep 1
+fi
+
+if [ -n "$ACTIVITY" ]; then
+  gum log --level info "Showing the current in_progress build"
+  platform activity:log $ACTIVITY
+else
+  gum log --level error "There's no push in progress to log"
+fi
